0aa1584ccf27aaad3beeb78b18edaf20fc237473
Kushang Gajjar <g.kushang@gmail.com>
2016-09-27 13:45:11 -0700

    calculate duration

--- test/features/step_definitions/stepDefs.js
+++ test/features/step_definitions/stepDefs.js
@@ -15,8 +15,7 @@ var stepDefs = function stepDefs() {
     });
 
     this.Given(/^Fred runs a passing cucumber scenario on behalf of "([^"]*)"/, function(name, callback) {
-        setTimeout(callback, 3000);
-        // callback();
+        setTimeout(callback, 2000);
     });
 
     this.Then(/^he has the JSON cucumber formatted file at the end of run$/, function(callback) {
