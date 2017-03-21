e784673d2b16d53a0e138b3f55b8fb6d482e52e0
andrey-rodkin <andrey.rodkin@gmail.com>
2017-03-21 15:13:59 +0300

    Fix for reports with screenshots for each step. Now only one screnshoot is expanded when we click on "Screenshot +" link. In previous variant more then one screnshot could be expanded by one click

--- templates/bootstrap/script.js
+++ templates/bootstrap/script.js
@@ -10,10 +10,10 @@ $(document).ready(function() {
     $('a.toggle').on('click', function() {
         if ($(this).text() === 'Screenshot -') {
             $(this).text('Screenshot +');
-            $(this).siblings('a.screenshot').find('img').hide();
+            $(this).next('a.screenshot').find('img').hide();
         } else {
             $(this).text('Screenshot -');
-            $(this).siblings('a.screenshot').find('img').show();
+            $(this).next('a.screenshot').find('img').show();
         }
     });
     var $generated = $('.generated-on');
