b5b1b609e9279b4d2dcd6717109d64049c053826
gkushang <gkushang@gmail.com>
2017-06-08 20:16:20 -0400

    uncomment the attach plain text

--- test/features/step_definitions/hooks.js
+++ test/features/step_definitions/hooks.js
@@ -17,10 +17,10 @@ defineSupportCode(function({After, Before, registerHandler}) {
     });
 
     Before({tags: '@testPassing'}, function (scenario, callback) {
-        // this.attach('Tests INFO will print here.<br>To attach INFO to Any steps, use scenario.attach function in your step definitions as shown below.<br><br>If you pass HTML\'s to scenario.attach then reporter will format accordingly <br>' +
-        //     '<br>Simple String  : scenario.attach(\'sample data\')' +
-        //     '<br>Pretty JSON    : scenario.attach(JSON.stringify(json, null, 2))' +
-        //     '<br>HTML Link      : scenario.attach(\'format the link with html-a tag\'', 'application/json');
+        this.attach('Tests INFO will print here.<br>To attach INFO to Any steps, use scenario.attach function in your step definitions as shown below.<br><br>If you pass HTML\'s to scenario.attach then reporter will format accordingly <br>' +
+            '<br>Simple String  : scenario.attach(\'sample data\')' +
+            '<br>Pretty JSON    : scenario.attach(JSON.stringify(json, null, 2))' +
+            '<br>HTML Link      : scenario.attach(\'format the link with html-a tag\'', 'application/json');
 
         this.attach('some text');
         callback();
