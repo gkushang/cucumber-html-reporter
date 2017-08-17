23be31c4e5735b06b8125289ca95d52d86a17a26
John Matos <rDr4g0n@gmail.com>
2017-08-17 10:27:35 -0500

    remove accordian stuff as it doesnt apply to simpler template

--- templates/simpler/script.js
+++ templates/simpler/script.js
@@ -1,32 +1,3 @@
-window.onload = function() {
-
-    //  Accordion hide/show
-    var accordionTitles = document.getElementsByClassName('accordion-title');
-
-    //  Convert node list to array
-    Array.prototype.slice.call(accordionTitles).forEach(function(title) {
-
-        title.onclick = function() {
-
-            var content = title.nextElementSibling;
-            var style = window.getComputedStyle(content);
-            var display = style.getPropertyValue('display');
-
-            if (display === 'block') {
-                content.style.display = 'none';
-            } else {
-                content.style.display = 'block';
-            }
-
-            return false;
-        }
-    });
-
-    // Update build time to since
-    // var buildTimeElem = document.getElementById("buildTime");
-    // buildTimeElem.innerHTML = 'Built ' + moment(buildTimeElem.innerHTML).fromNow();
-};
-
 function toggleScreenshot(e){
     if(this.innerText === "Screenshot -"){
         this.innerText = "Screenshot +";
