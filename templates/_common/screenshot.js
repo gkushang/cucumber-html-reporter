7dd073d73967af43efe059a0aa27d1cb4b1395f3
gkushang <gkushang@gmail.com>
2018-06-28 19:54:24 -0400

    toggle info

--- templates/_common/screenshot.js
+++ templates/_common/screenshot.js
@@ -1,8 +1,21 @@
 $('a.toggle').on('click', function() {
+
+    if ($(this).text() === 'Show Info -') {
+        $(this).text('Show Info +');
+    } else if ($(this).text() === 'Show Info +') {
+        $(this).text('Show Info -');
+    }
+
+    if ($(this).text() === 'Show Error -') {
+        $(this).text('Show Error +');
+    } else if ($(this).text() === 'Show Error +') {
+        $(this).text('Show Error -');
+    }
+
     if ($(this).text() === 'Screenshot -') {
         $(this).text('Screenshot +');
         $(this).next('a.screenshot').find('img').hide();
-    } else {
+    } else if($(this).text() === 'Screenshot +') {
         $(this).text('Screenshot -');
         $(this).next('a.screenshot').find('img').show();
     }
