2a369d310eec2b201fb8ee48b74ab38985d90b23
gkushang <gkushang@gmail.com>
2018-06-28 20:50:33 -0400

    refactor

--- templates/_common/screenshot.js
+++ templates/_common/screenshot.js
@@ -1,22 +1,16 @@
 $('a.toggle').on('click', function() {
 
-    if ($(this).text() === 'Show Info -') {
-        $(this).text('Show Info +');
-    } else if ($(this).text() === 'Show Info +') {
-        $(this).text('Show Info -');
-    }
-
-    if ($(this).text() === 'Show Error -') {
-        $(this).text('Show Error +');
-    } else if ($(this).text() === 'Show Error +') {
-        $(this).text('Show Error -');
-    }
-
     if ($(this).text() === 'Screenshot -') {
-        $(this).text('Screenshot +');
+        // $(this).text('Screenshot +');
         $(this).next('a.screenshot').find('img').hide();
     } else if($(this).text() === 'Screenshot +') {
-        $(this).text('Screenshot -');
+        // $(this).text('Screenshot -');
         $(this).next('a.screenshot').find('img').show();
+    }
+
+    if ($(this).text().includes(' -')) {
+        $(this).text($(this).text().replace(' -', ' +'));
+    } else {
+        $(this).text($(this).text().replace(' +', ' -'));
     }
 });
